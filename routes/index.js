c87ad498bc8ab8755c8d6473e07508932bc8d011
JosephCollins Udeani <JosephCollins.Udeani@aa.com>
2020-11-02 04:16:40 -0600

    added images

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,21 @@
 var express = require('express');
 var router = express.Router();
-var {MongoClient} = require('mongodb');
+var { MongoClient } = require('mongodb');
 
 const title = process.env.TITLE;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   const URL = process.env.URL;
   const dbName = 'Library';
-  (async function mongo(){
+  (async function mongo() {
     let client;
-    try{
+    try {
       client = await MongoClient.connect(URL);
-      const db=client.db(dbName);
+      const db = client.db(dbName);
       const response = await db.collection('books').find().toArray();
-      res.json(response);
-    }catch(err){
+      res.render('index', { books: response });
+    } catch (err) {
       console.log(err);
     }
   }());
