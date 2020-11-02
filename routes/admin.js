5b659b4871bd41a62fcce8d4e048f77d731e1f64
JosephCollins Udeani <JosephCollins.Udeani@aa.com>
2020-11-02 02:33:43 -0600

    added mongo client

---
+++ routes/admin.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+var { MongoClient } = require('mongodb');
+var books = require('./../models/books');
+
+const title = process.env.TITLE;
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  const URL = '2I5fBT5nLyeDcvGnOiVeHgdecsStzqE2a21FI7uuEz2d83HQQzXBHygPiaElDJz69hdQ2sdVV48TnrBA5h4oWQ==';
+  const dbName = 'Library';
+  (async function mongo() {
+    let client;
+    try {
+      client = await MongoClient.connect(URL);
+      const db = client.db(dbName);
+      const response = await db.collection('books').insertMany(books);
+      res.json(response);
+    } catch (err) {
+      console.log(err);
+    }
+  }());
+});
+
+module.exports = router;
