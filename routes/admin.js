d715a0851170ff54a96609cd3d5c4d4c25980df4
JosephCollins Udeani <JosephCollins.Udeani@aa.com>
2020-11-02 03:11:34 -0600

    refabcer

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,58 @@
 var express = require('express');
 var router = express.Router();
 var { MongoClient } = require('mongodb');
-var books = require('./../models/books');
 
+const books = [
+  {
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolayevich Tolstoy',
+    bookId: 656,
+    read: false
+  },
+  {
+    title: 'Les Misérables',
+    genre: 'Historical Fiction',
+    author: 'Victor Hugo',
+    bookId: 24280,
+    read: false
+  },
+  {
+    title: 'The Time Machine',
+    genre: 'Science Fiction',
+    author: 'H. G. Wells',
+    read: false
+  },
+  {
+    title: 'A Journey into the Center of the Earth',
+    genre: 'Science Fiction',
+    author: 'Jules Verne',
+    read: false
+  },
+  {
+    title: 'The Dark World',
+    genre: 'Fantasy',
+    author: 'Henry Kuttner',
+    read: false
+  },
+  {
+    title: 'The Wind in the Willows',
+    genre: 'Fantasy',
+    author: 'Kenneth Grahame',
+    read: false
+  },
+  {
+    title: 'Life On The Mississippi',
+    genre: 'History',
+    author: 'Mark Twain',
+    read: false
+  },
+  {
+    title: 'Childhood',
+    genre: 'Biography',
+    author: 'Lev Nikolayevich Tolstoy',
+    read: false
+  }]
 const title = process.env.TITLE;
 /* GET home page. */
 router.get('/', function (req, res, next) {
