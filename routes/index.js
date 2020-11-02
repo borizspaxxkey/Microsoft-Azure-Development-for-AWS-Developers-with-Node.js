3585512861e25f781fc595d0aa585e4f798813a7
JosephCollins Udeani <JosephCollins.Udeani@aa.com>
2020-11-02 05:41:03 -0600

    appinfight key change

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,23 @@
 var express = require('express');
 var router = express.Router();
-var { MongoClient } = require('mongodb');
+var {MongoClient} = require('mongodb');
 const appInsights = require('applicationinsights');
+
 const title = process.env.TITLE;
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', function(req, res, next) {
   const URL = process.env.URL;
   const dbName = 'Library';
-  (async function mongo() {
+  (async function mongo(){
     let client;
-    try {
+    try{
       client = await MongoClient.connect(URL);
-      const db = client.db(dbName);
+      const db=client.db(dbName);
       const response = await db.collection('books').find().toArray();
-      appInsights.defaultClient.trackEvent({ name: 'mongoRequest', properties: { dbname, size: response.length } });
-      res.render('index', { books: response });
-    } catch (err) {
+      appInsights.defaultClient.trackEvent({name: 'mongoRequest', properties: {dbName, size: response.length}});
+      res.render('index', {books: response});
+    }catch(err){
       console.log(err);
     }
   }());
